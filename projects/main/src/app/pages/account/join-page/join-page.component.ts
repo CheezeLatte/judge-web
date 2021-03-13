4eb2bdacb22d3df404c125b4d03e4f7fe28170ad
vcho1958 <gusckd57@gmail.com>
2021-03-13 22:28:53 +0900

    some fix

--- projects/main/src/app/pages/account/join-page/join-page.component.ts
+++ projects/main/src/app/pages/account/join-page/join-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter} from '@angular/core';
 import { FormBuilder} from '@angular/forms';
 interface formObjects {
   id: string,
@@ -9,6 +9,11 @@ interface formObjects {
   email: string,
   affiliation: string
 }
+
+interface formClasses{
+  class: string,
+  korean: string
+}
 @Component({
   selector: 'sw-join-page',
   templateUrl: './join-page.component.html',
@@ -17,6 +22,15 @@ interface formObjects {
 export class JoinPageComponent implements OnInit {
   joinForm;
   formSelected = new EventEmitter();
+  formClass:Array<formClasses> = [
+    {class : 'id', korean:'아이디'},
+    {class : 'password', korean:'비밀번호'},
+    {class : 'password', korean:'비밀번호 확인'},
+    {class : 'name', korean:'이름'},
+    {class : 'gradeNum', korean:'학번'},
+    {class : 'affiliation', korean:'소속'},
+    {class : 'email', korean:'E-mail'}
+  ];
   constructor(
     private formBuilder: FormBuilder,
     ){
@@ -28,9 +42,13 @@ export class JoinPageComponent implements OnInit {
         email: '',
         affiliation: ''
       })
+
+
     }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
+
+
   }
   onSubmit(value:string ){
 
