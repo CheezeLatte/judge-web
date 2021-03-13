1904c1e4a9ff98afaff05a59cf64034cab533500
vcho1958 <gusckd57@gmail.com>
2021-03-13 18:13:02 +0900

    add branch (feature/join)

--- projects/main/src/app/pages/account/join-page/join-page.component.ts
+++ projects/main/src/app/pages/account/join-page/join-page.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
+interface formObjects {
+  id: string,
+  name: string,
+  password: string,
+  confirmPassword: string,
+  gradeNum: string,
+  email: string,
+  affiliation: string
+}
 @Component({
   selector: 'sw-join-page',
   templateUrl: './join-page.component.html',
   styleUrls: ['./join-page.component.scss']
 })
 export class JoinPageComponent implements OnInit {
-
-  constructor() { }
-
+  joinForm;
+  formSelected = new EventEmitter();
+  constructor(
+    private formBuilder: FormBuilder,
+    ){
+      this.joinForm = this.formBuilder.group({
+        id: '',
+        name: '',
+        password: '',
+        gradeNum: '',
+        email: '',
+        affiliation: ''
+      })
+    }
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+  onSubmit(value:string ){
 
+  }
 }
